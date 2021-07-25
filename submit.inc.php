a638410dace0c048ee1a49c3c879b02870352262
Peem <mrpeem@gmail.com>
2021-07-25 07:23:05 -0700

    test affected rows

--- submit.inc.php
+++ submit.inc.php
@@ -5,7 +5,9 @@
   $description = $_POST['description'];
   $price = $_POST['price'];
 
+  printf(mysql_affected_rows($conn));
   $sql = "INSERT INTO product (name, description, price) VALUES ('$name', '$description', '$price');";
   mysqli_query($conn, $sql);
+  printf(mysql_affected_rows($conn));
 
   header("Location: /index.php?input=success");
