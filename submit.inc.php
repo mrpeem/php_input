f0f926aa99d500daf2a9cf06a89a30e0e8962e40
Peem <mrpeem@gmail.com>
2021-07-25 09:41:31 -0700

    error msg

--- submit.inc.php
+++ submit.inc.php
@@ -8,6 +8,11 @@
   $sql = "INSERT INTO product (name, description, price) VALUES ('$name', '$description', '$price');";
   mysqli_query($conn, $sql);
 
-  // header("Location: /index.php?submit=success");
-  echo '<script type="text/javascript">alert("Success");window.location.href="/index.php";</script>';
+  if ($sql) {
+    echo '<script type="text/javascript">alert("Success");window.location.href="/index.php";</script>';  
+  }
+  else {
+    echo '<script type="text/javascript">alert("Error");window.location.href="/index.php";</script>';
+  }
+  
 
