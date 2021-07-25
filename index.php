6ccbe07c331cbf7a9773d5b3c8ec918fed23f893
Peem <mrpeem@gmail.com>
2021-07-25 07:13:59 -0700

    script to add to database

--- index.php
+++ index.php
@@ -7,27 +7,20 @@
 </head>
 <body>
   $conn;
-  <form action="index.php" method="post">
-    
+  <form action="submit.inc.php" method="post">
     <div>
       <label for="name">Name</label>
-      <input type="text" name="name">
+      <input type="text" name="name" required>
     </div>
-
     <div>
       <label for="description">Description</label>
-      <input type="text" name="description">
+      <input type="text" name="description" required>
     </div>
-
     <div>
       <label for="price">Price</label>
-      <input type="text" name="price">
+      <input type="number" step="any" name="price" required>
     </div>
-
-    <button type="submit"> Submit </button>
-    
-
-
+    <button type="submit" name="submit"> Submit </button>
   </form>
 </body>
 </html>
---
+++ submit.inc.php
@@ -0,0 +1,9 @@
+<?php
+  include_once 'db.inc.php';
+
+  $name = $_POST['name'];
+  $description = $_POST['description'];
+  $price = $_POST['price'];
+
+  $sql = "INSERT INTO product (name, description, price) VALUES ('$name', '$description', '$price');";
+  mysqli_query($conn, $sql);
