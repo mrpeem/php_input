af8df89a1d638ad42c933079130ca26e36ea5e91
Peem <mrpeem@gmail.com>
2021-07-24 21:07:26 -0700

    form?

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
-  echo "heroku test"
+  include_once 'db.inc.php';
 ?>
+
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+  $conn;
+  <form action="index.php" method="post">
+    
+    <div>
+      <label for="name">Name</label>
+      <input type="text" name="name">
+    </div>
+
+    <div>
+      <label for="description">Description</label>
+      <input type="text" name="description">
+    </div>
+
+    <div>
+      <label for="price">Price</label>
+      <input type="text" name="price">
+    </div>
+
+    <button type="submit"> Submit </button>
+    
+
+
+  </form>
+</body>
+</html>
